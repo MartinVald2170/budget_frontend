25f564a39fc61883e34da2e3d4ebefbdb7401c1e
MartinVald2170 <noways2170@gmail.com>
2020-07-30 03:15:18 +1000

    landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,16 +1,32 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import Button from "react-bootstrap/Button"
+import "./LandingPage.css"
 
 const LandingPage = () => {
 
     return(
-        <div>
-          <h2>Landing Page</h2>  
-          <Link to={`/AllSavings`}>View All Saving Goals</Link>
-          <br></br>
-          <Link to={`/AllExpenses`}>View All Expenses</Link>
+      <div>
+          <h2>Btrackr</h2>
+          <div>
+          <Link to={`/AllSavings`}>
+          <Button variant="primary" size="lg">
+            View All Saving Goals
+            </Button>
+            </Link>
+            </div>
+            <br></br>
+            <div>
+          <Link to={`/AllExpenses`}>
+          <Button variant="primary" size="lg">
+            View All Expenses
+            </Button>
+            </Link>
+            </div>
+          
+          
         </div>
-        
+       
     )
 
 }
